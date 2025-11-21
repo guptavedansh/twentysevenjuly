e35db7626ee5988207a7aac2ed5d76363b09abb1
Vedansh Gupta <vedansh2002.gupta@gmail.com>
2025-11-21 15:25:00 +0530

    Added Login and Logout Page, yet to add the functionality

--- twentysevenjuly/settings.py
+++ twentysevenjuly/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'crispy_bootstrap4',
+    'users.apps.UsersConfig',
     'birthday_app.apps.BirthdayAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- twentysevenjuly/urls.py
+++ twentysevenjuly/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as authViews
+from users import views as userViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('birthday_app.urls')),
+    path('login/', authViews.LoginView.as_view(template_name = 'users/login.html'), name='login'),
+    path('logout/', authViews.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
 ]
